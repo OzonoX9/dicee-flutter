2e238ac9eb7d4ff7b7506144550ceedbe1bbdd10
Jin Ozono <ozononicholas@gmail.com>
2023-11-16 22:14:08 -0300

    project completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,6 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          elevation: 0,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
@@ -15,9 +17,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void rollDices() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                onPressed: () => rollDices(),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                onPressed: () => rollDices(),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
